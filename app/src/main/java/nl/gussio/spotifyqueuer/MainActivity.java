edf9fd26ff996430eb48a87478c53c2d309e267a
Jelle <jellehulter@gmail.com>
2018-09-06 19:46:27 +0200

    Fixed issue with Jackson (even though I don't use it, idk)

--- app/src/main/java/nl/gussio/spotifyqueuer/MainActivity.java
+++ app/src/main/java/nl/gussio/spotifyqueuer/MainActivity.java
@@ -2,15 +2,18 @@ package nl.gussio.spotifyqueuer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.mappers.gson.GsonMapper;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String CLIENT_ID = "51c1a7b0c4bc499698b10eb15bfeaad3";
     private static final String REDIRECT_URI = "nl.gussio.spotifyqueuer://callback";
-    private SpotifyAppRemote mSpotifyAppRemote;
+    private SpotifyAppRemote remote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +24,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        ConnectionParams connectionParams = new ConnectionParams.Builder(CLIENT_ID)
+        final ConnectionParams connectionParams = new ConnectionParams.Builder(CLIENT_ID)
                 .setRedirectUri(REDIRECT_URI)
                 .showAuthView(true)
+                .setJsonMapper(GsonMapper.create())
                 .build();
+        SpotifyAppRemote.CONNECTOR.connect(this, connectionParams, new Connector.ConnectionListener() {
+            @Override
+            public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                remote = spotifyAppRemote;
+                Log.d("Debugger", "Connected to Spotfiy.");
+                connected();
+            }
 
+            @Override
+            public void onFailure(Throwable throwable) {
+                Log.e("Debugger", throwable.getMessage(), throwable);
+            }
+        });
     }
 
     private void connected() {
-        // Then we will write some more code here.
+        remote.getPlayerApi().play("spotify:track:6tv43DiBgTHgC0dtID2MXW");
     }
 
     @Override
