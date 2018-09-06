83250dddfc7afb61bbda0e9c6d54bf80b8eecd38
Jelle <jellehulter@gmail.com>
2018-09-06 18:48:09 +0200

    First commit.

---
+++ app/src/main/java/nl/gussio/spotifyqueuer/MainActivity.java
@@ -0,0 +1,29 @@
+package nl.gussio.spotifyqueuer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // We will start writing our code here.
+    }
+
+    private void connected() {
+        // Then we will write some more code here.
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Aaand we will finish off here.
+    }
+}
