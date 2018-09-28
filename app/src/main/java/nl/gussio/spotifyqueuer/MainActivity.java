b892941f73af79bc07fef1e18f1d818527f010b4
Jelle <jellehulter@gmail.com>
2018-09-28 22:14:24 +0200

    Fixed unused imports.

--- app/src/main/java/nl/gussio/spotifyqueuer/MainActivity.java
+++ app/src/main/java/nl/gussio/spotifyqueuer/MainActivity.java
@@ -1,14 +1,9 @@
 package nl.gussio.spotifyqueuer;
 
-import android.app.AlarmManager;
 import android.app.AlertDialog;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
