238362269d4b92af30b779ba09b2af8c890d2b5f
Jelle Hulter <jellehulter@gmail.com>
2019-01-19 00:42:20 +0100

    Created activity and XML files for the slides.

---
+++ app/src/main/java/nl/gussio/trackqueuer/IntroActivity.java
@@ -0,0 +1,26 @@
+package nl.gussio.trackqueuer;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private PagerAdapter myViewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+    private Button btnSkip, btnNext;
+    private PrefManager prefManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
